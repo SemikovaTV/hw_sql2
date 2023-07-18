90d79dfeff70a816c4fb9646afb91693ee005663
SemikovaTV <129278233+SemikovaTV@users.noreply.github.com>
2023-07-18 11:25:46 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -2,16 +2,18 @@ SELECT s.last_name, s.first_name,c.city, COUNT(c2.first_name)
 FROM staff s
 JOIN address a ON a.address_id = s.address_id
 JOIN city c ON c.city_id = a.city_id
-JOIN customer c2 ON c2.customer_id =c2.customer_id
+JOIN customer c2 ON s.store_id = c2.store_id
 GROUP BY s.last_name , c.city ,s.first_name
 HAVING COUNT(c2.customer_id) > 300;
+
 
 SELECT COUNT(`length`) film_id
 FROM film f
 WHERE `length` > (SELECT AVG(`length`) FROM film)
 
-SELECT MONTH(payment_date), COUNT(rental_id), SUM(amount)
+SELECT DATE_FORMAT(`payment_date`, '%M %Y'), COUNT(rental_id), SUM(amount)
 FROM payment
-GROUP BY MONTH(payment_date)
+GROUP BY DATE_FORMAT(`payment_date`, '%M %Y')
 ORDER BY SUM(amount) DESC
 LIMIT 1
+
